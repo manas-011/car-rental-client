43f3073f0df702414ee3a0cdc037613ad2b81ec9
manas-011 <109292653+manas-011@users.noreply.github.com>
2023-09-03 15:14:05 +0530

    Update bookingActions.jsx

--- src/redux/actions/bookingActions.jsx
+++ src/redux/actions/bookingActions.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { message } from "antd";
 
+
+
 export const bookCar = (reqObj) => async (dispatch) => {
   dispatch({ type: "LOADING", payload: true });
 
   try {
-    const res = await axios.post("/api/bookings/bookcar" , reqObj);
+    const res = await axios.post("https://car-rental-server-orpin.vercel.app/api/bookings/bookcar" , reqObj);
 
     dispatch({ type: "LOADING", payload: false });
     message.success("Your car booked successfully");
@@ -29,7 +31,7 @@ export const getAllBookings=()=> async dispatch=>{
   dispatch({type: 'LOADING' , payload:true}) ;
 
   try {
-      const response = await axios.get('/api/bookings/getallbookings') ;
+      const response = await axios.get('https://car-rental-server-orpin.vercel.app/api/bookings/getallbookings') ;
       dispatch({type: 'GET_ALL_BOOKINGS', payload:response.data})
       dispatch({type: 'LOADING' , payload:false}) 
 
