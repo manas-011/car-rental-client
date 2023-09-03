a60f607e4efe706cd73cc4e291283297f08ee678
manas-011 <109292653+manas-011@users.noreply.github.com>
2023-09-03 15:14:48 +0530

    Update carsActions.jsx

--- src/redux/actions/carsActions.jsx
+++ src/redux/actions/carsActions.jsx
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import { message } from 'antd';
 
+
 export const getAllCars=()=> async dispatch =>{
 
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-        const response = await axios.get('/api/cars/getallcars')
+        const response = await axios.get('https://car-rental-server-orpin.vercel.app/api/cars/getallcars')
         dispatch({type: 'GET_ALL_CARS', payload:response.data})
         dispatch({type: 'LOADING' , payload:false})
 
@@ -25,7 +26,7 @@ export const addCar=(reqObj)=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-         await axios.post('/api/cars/addcar' , reqObj)
+         await axios.post('https://car-rental-server-orpin.vercel.app/api/cars/addcar' , reqObj)
        
          dispatch({type: 'LOADING' , payload:false})
          message.success('New car added successfully')
@@ -47,7 +48,7 @@ export const editCar = (reqObj) => async dispatch=>{
 
     try {
         
-         await axios.post('/api/cars/editcar' , reqObj)
+         await axios.post('https://car-rental-server-orpin.vercel.app/api/cars/editcar' , reqObj)
        
          dispatch({type: 'LOADING' , payload:false})
          message.success('Car details updated successfully')
@@ -68,7 +69,7 @@ export const deleteCar = (reqObj) => async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-         await axios.post('/api/cars/deletecar' , reqObj)
+         await axios.post('https://car-rental-server-orpin.vercel.app/api/cars/deletecar' , reqObj)
        
          dispatch({type: 'LOADING' , payload:false}) ;
 
